0b366333610963b342a52661423cdfd0502f1cff
Brandon Ketterman <brandonkett24@gmail.com>
2020-09-11 10:49:17 -0400

    created schema and seeds.sql files

---
+++ db/schema.sql
@@ -0,0 +1,11 @@
+DROP DATABASE IF EXISTS burgers_db;
+CREATE DATABASE burgers_db;
+
+USE burgers_db;
+
+CREATE TABLE burgers(
+id INT NOT NULL auto_increment,
+burger_name varchar(255) not null,
+devoured BOOLEAN DEFAULT false,
+PRIMARY KEY(id)
+);
---
+++ db/seeds.sql
@@ -0,0 +1,2 @@
+INSERT INTO burgers (burger_name) VALUES ('plain cheeseburger'), ('bacon cheeseburger'), ('big mac');
+INSERT INTO burgers (burger_name, devoured) VALUES ('quarter pounder, true'), ('steak burger', false), ('hamburger', true);
